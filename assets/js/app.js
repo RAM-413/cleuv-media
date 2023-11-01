2811b5c810315fc9d83a31eee6bf5a6d64bf0edc
RAM-413 <ramsharma22422@gmail.com>
2023-11-01 16:50:08 +0530

    modify

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 $('.slider-1').slick({
   dots: false,
   infinite: true,
@@ -80,4 +81,48 @@ $('.slider-3').slick({
       }
     },
   ]
+});
+
+
+
+let nav = document.querySelector(".nav");
+let menubar = document.querySelector(".menubar");
+let loadtake = document.querySelector(".nav-logo")
+let finish = document.querySelectorAll(".finish");
+let overlay = document.querySelector(".over-layer")
+let bgbody = document.querySelector(".bg-bodys");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
+nav.addEventListener("click", function () {
+    menubar.classList.toggle("left-0")
+    nav.classList.toggle("nav-bg")
+    nav.classList.toggle("nav-2")
+    span1.classList.toggle("nav1")
+    span2.classList.toggle("nav2")
+    span3.classList.toggle("nav3")
+    overlay.classList.toggle("left-0")
+    bgbody.classList.toggle("over-flow")
+})
+overlay.addEventListener("click", function () {
+    menubar.classList.toggle("left-0")
+    span1.classList.toggle("nav1")
+    nav.classList.toggle("nav-bg")
+    span2.classList.toggle("nav2")
+    span3.classList.toggle("nav3")
+    overlay.classList.toggle("left-0")
+    bgbody.classList.toggle("over-flow")
+})
+finish.forEach(e => {
+    e.addEventListener("click", function () {
+        menubar.classList.remove("left-0")
+        span1.classList.toggle("nav1")
+        nav.classList.toggle("nav-bg")
+        span2.classList.toggle("nav2")
+        span3.classList.toggle("nav3")
+        bgbody.classList.remove("over-flow")
+        overlay.classList.toggle("left-0")
+        span2.classList.toggle("nav2")
+        span3.classList.toggle("nav3")
+    })
 });
