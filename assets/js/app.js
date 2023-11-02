a7540875e4d4ade517b71d06254bd56aa38cc6a4
RAM-413 <ramsharma22422@gmail.com>
2023-11-02 11:36:04 +0530

    modify

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,24 @@
+const preloader = document.getElementById("preloader");
+setTimeout(() => {
+  document.getElementById("preloder").classList.add("d-none");
+  document.body.classList.remove("overflow-hidden")
+}, 1000);
+
+
+let topIcon = document.getElementById("top");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    topIcon.style.display = "flex";
+  }
+  else {
+    topIcon.style.display = "none";
+  }
+});
+
+function up() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 $('.slider-1').slick({
   dots: false,
@@ -12,9 +33,11 @@ $('.slider-1').slick({
       breakpoint: 992,
       settings: {
         slidesToShow: 1.75,
+        dots: true,
         speed: 500,
         infinite: true,
         slidesToScroll: 2
+
       }
     },
     {
@@ -29,24 +52,67 @@ $('.slider-1').slick({
 
 $('.slider-2').slick({
   infinite: true,
-  speed: 300,
-  slidesToShow: 1.75,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
   autoplay: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '400px',
+  arrows: false,
   responsive: [
+
     {
-      breakpoint: 768,
+      breakpoint: 1200,
+      settings: {
+        centerMode: true,
+        centerPadding: '300px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        centerMode: true,
+        centerPadding: '300px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        centerMode: true,
+        centerPadding: '200px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 600,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+      }
 
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       }
     },
-
   ]
-});
+}
+);
+
 $('.slider-3').slick({
   dots: true,
   infinite: true,
@@ -95,34 +161,34 @@ let span1 = document.querySelector(".span1");
 let span2 = document.querySelector(".span2");
 let span3 = document.querySelector(".span3");
 nav.addEventListener("click", function () {
-    menubar.classList.toggle("left-0")
-    nav.classList.toggle("nav-bg")
-    nav.classList.toggle("nav-2")
-    span1.classList.toggle("nav1")
-    span2.classList.toggle("nav2")
-    span3.classList.toggle("nav3")
-    overlay.classList.toggle("left-0")
-    bgbody.classList.toggle("over-flow")
+  menubar.classList.toggle("left-0")
+  nav.classList.toggle("nav-bg")
+  nav.classList.toggle("nav-2")
+  span1.classList.toggle("nav1")
+  span2.classList.toggle("nav2")
+  span3.classList.toggle("nav3")
+  overlay.classList.toggle("left-0")
+  bgbody.classList.toggle("over-flow")
 })
 overlay.addEventListener("click", function () {
-    menubar.classList.toggle("left-0")
+  menubar.classList.toggle("left-0")
+  span1.classList.toggle("nav1")
+  nav.classList.toggle("nav-bg")
+  span2.classList.toggle("nav2")
+  span3.classList.toggle("nav3")
+  overlay.classList.toggle("left-0")
+  bgbody.classList.toggle("over-flow")
+})
+finish.forEach(e => {
+  e.addEventListener("click", function () {
+    menubar.classList.remove("left-0")
     span1.classList.toggle("nav1")
     nav.classList.toggle("nav-bg")
     span2.classList.toggle("nav2")
     span3.classList.toggle("nav3")
+    bgbody.classList.remove("over-flow")
     overlay.classList.toggle("left-0")
-    bgbody.classList.toggle("over-flow")
-})
-finish.forEach(e => {
-    e.addEventListener("click", function () {
-        menubar.classList.remove("left-0")
-        span1.classList.toggle("nav1")
-        nav.classList.toggle("nav-bg")
-        span2.classList.toggle("nav2")
-        span3.classList.toggle("nav3")
-        bgbody.classList.remove("over-flow")
-        overlay.classList.toggle("left-0")
-        span2.classList.toggle("nav2")
-        span3.classList.toggle("nav3")
-    })
+    span2.classList.toggle("nav2")
+    span3.classList.toggle("nav3")
+  })
 });
